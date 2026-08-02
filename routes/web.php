c5449f1b21e99ce7150fa0140eafbe3eab8a8e39
Milton Ytusaca <milton.yv98@gmail.com>
2026-08-02 17:43:20 -0500

    docker y nginx

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get("/render", function () {
+    return response()->json([
+        "status" => "ok",
+        "app" => config("app.name"),
+    ]);
+});
