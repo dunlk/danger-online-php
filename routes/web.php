4b47ebd9b2e6ad8dbb0d2ab2903d162402872f22
mariacueva <maria.cueva@tecsup.edu.pe>
2026-08-03 14:18:48 -0500

    agregando auth admin-middleware

--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,31 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/health', function () {
-    return response()->json([
-        'status' => 'ok',
-    ]);
-});
-
 Route::get('/', function () {
-    return "Hola desde home";
+    return view('welcome');
 });
 
-Route::get("/render", function () {
-    return "Hola desde render";
+Route::middleware(['auth', 'admin'])
+    ->prefix('admin')
+    ->group(function () {
+        Route::get('/dashboard', function () {
+            return "Panel de administración";
+        })->name('admin.dashboard');
+    });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+
+
+
+require __DIR__ . '/auth.php';
