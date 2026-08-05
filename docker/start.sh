24b3203d116c5782084cc8b03c436641e46f3719
Milton Ytusaca <milton.yv98@gmail.com>
2026-08-04 23:32:39 -0500

    docker

--- docker/start.sh
+++ docker/start.sh
@@ -15,10 +15,11 @@ php artisan config:clear
 
 php artisan migrate --force
 
+php artisan storage:link || true
+
 php artisan config:cache
 php artisan route:cache
 php artisan view:cache
-php artisan storage:link || true
 
 php-fpm -D
 
